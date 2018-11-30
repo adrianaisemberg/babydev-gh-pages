b4bcdcff63fcee45475d62159927776d4afb7934
Adrian Aisemberg <adrianaisemberg@gmail.com>
2018-11-29 21:34:02 -0800

    Updates

---
+++ precache-manifest.61d49ef4893a303c4e0972c2be09e2cd.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "d09166fb0ecf833d58cfc96f42bbb341",
+    "url": "./static/media/milestones.d09166fb.jpg"
+  },
+  {
+    "revision": "c2ccfa467913b0fae44f",
+    "url": "./static/css/main.0fd210f5.chunk.css"
+  },
+  {
+    "revision": "40f99fbcc095fbdf491f",
+    "url": "./static/js/1.40f99fbc.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "5da3a02b459752db5c64e73a245cd12c",
+    "url": "./static/media/logo_625x242.5da3a02b.png"
+  },
+  {
+    "revision": "c2ccfa467913b0fae44f",
+    "url": "./static/js/main.c2ccfa46.chunk.js"
+  },
+  {
+    "revision": "bf383ea11d9216ecfdf27555cef29a35",
+    "url": "./static/media/infant-massage.bf383ea1.jpg"
+  },
+  {
+    "revision": "2b9a8d0e23865e255cc013b8a8cfc61a",
+    "url": "./static/media/services.2b9a8d0e.jpg"
+  },
+  {
+    "revision": "64641f54198f5669e5b05d2a66cfb977",
+    "url": "./static/media/method.64641f54.jpg"
+  },
+  {
+    "revision": "3c074ae1a2667848cd29179552bb3f72",
+    "url": "./static/media/about-me.3c074ae1.jpg"
+  },
+  {
+    "revision": "882142c56083bc23c0c39975448075d1",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/babydev-gh-pages/precache-manifest.b1c480302427055652e20c1294e73deb.js"
+  "./precache-manifest.61d49ef4893a303c4e0972c2be09e2cd.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/babydev-gh-pages/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
