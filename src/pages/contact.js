b67b50fb4faa661257e50df7a63932b829d77396
Adrian Aisemberg <adrianaisemberg@gmail.com>
2018-12-05 23:40:13 -0800

    contact

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -11,20 +11,16 @@ export class Contact extends React.Component {
           <input type="hidden" name="_subject" value="BabyDevStudio Contact" />
           <input type="hidden" name="_cc" value="adrianaisemberg@gmail.com" />
           <div className="input">
-            <label htmlFor='form.name'>Name:</label>
+            <label htmlFor='form.name'>Name</label>
             <div><input type='text' name='name' id='form.name' /></div>
           </div>
           <div className="input">
-            <label htmlFor='form.email'>Email:</label>
+            <label htmlFor='form.email'>Email</label>
             <div><input type='email' name='email' id='form.email' /></div>
           </div>
           <div className="input">
-            <label htmlFor='form.phone'>Phone:</label>
-            <div><input type='tel' id='form.phone' /></div>
-          </div>
-          <div className="input">
-            <label htmlFor='form.text'>Message:</label>
-            <div><textarea name='message' id='form.message' rows={10} /></div>
+            <label htmlFor='form.text'>Message (please include your phone number)</label>
+            <div><textarea name='message' id='form.message' rows={10}/></div>
           </div>
           <div className="input">
             <input type='submit' value='Send' />
