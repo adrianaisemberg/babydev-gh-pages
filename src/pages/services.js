ff7e3f000a7fce6e88bbd3657a8f26a8d06161a5
Adrian Aisemberg <adrianaisemberg@gmail.com>
2019-10-04 19:59:35 -0700

    vars

--- src/pages/services.js
+++ src/pages/services.js
@@ -1,13 +1,22 @@
 import * as React from 'react';
 import { ServiceLink } from '../controls/service-link';
 import './../main.scss';
+import './services.scss';
 var services = require('./services.json');
 var classNames = require('classnames');
 
 export class Services extends React.Component {
   render() {
     window.scrollTo(0, 0)
-    var serviceLink = services.map((service, index) => (
+    var headers = services.header.map((header, index) => (
+      <div key={index}>{header.text}</div>
+    ));
+
+    var intro = services.intro.map((intro, index) => (
+      <div key={index}>{intro.text}</div>
+    ));
+
+    var serviceLinks = services.services.map((service, index) => (
       <ServiceLink
         key={index}
         title={service.title}
@@ -21,7 +30,9 @@ export class Services extends React.Component {
     return (
       <>
         <h1>SERVICES</h1>
-        <div className={classNames("content", "column")}>{serviceLink}</div>
+        <div className={classNames("service-header")}>{headers}</div>
+        <div className={classNames("service-intro")}>{intro}</div>
+        <div className={classNames("content", "column")}>{serviceLinks}</div>
       </>
     );
   }
