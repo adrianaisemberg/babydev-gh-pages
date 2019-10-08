716b8b1a87803a9ad52cc91f285d3c533ce9ed23
Adrian Aisemberg <adrianaisemberg@gmail.com>
2019-10-07 19:08:45 -0700

    ga

--- src/app.js
+++ src/app.js
@@ -3,9 +3,18 @@ import { Header } from './header';
 import { Main } from './main';
 import './app.scss';
 import { Copyright } from './copyright';
+import ReactGA from 'react-ga';
+import { createBrowserHistory } from 'history';
+
+const history = createBrowserHistory();
+history.listen(location => {
+  ReactGA.set({ page: location.pathname }); // Update the user's current page
+  ReactGA.pageview(location.hash); // Record a pageview for the given page
+});
 
 export class App extends React.Component {
   render() {
+    this.initializeReactGA();
     return (
       <div>
         <Header />
@@ -13,5 +22,9 @@ export class App extends React.Component {
         <Copyright />
       </div>
     )
+  }
+
+  initializeReactGA() {
+    ReactGA.initialize('UA-149615220-1');
   }
 }
