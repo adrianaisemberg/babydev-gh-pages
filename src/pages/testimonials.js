8d8929d1f189f3d5a02a61690b92cd131aeb27c1
Adrian Aisemberg <adrianaisemberg@gmail.com>
2019-10-19 10:02:01 -0700

    test

--- src/pages/testimonials.js
+++ src/pages/testimonials.js
@@ -26,7 +26,7 @@ export class Testimonials extends React.Component {
     return testimonials.testimonials.map((testimonial, index) => (
       <div key={index} className={classNames("testimonial-container")}>
         <div className={classNames("testimonial")}>{this.renderTestimonial(testimonial.testimonial)}</div>
-        <div className={classNames("testimonial-separator")}></div>
+        { index < testimonials.testimonials.length - 1 ? <div className={classNames("testimonial-separator")}></div> : null }
       </div>
     ));
   }
