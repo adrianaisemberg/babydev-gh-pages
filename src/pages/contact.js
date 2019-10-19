d8a6d91e2c0fdae8ec78e79cc5c41485de6d70e2
Adrian Aisemberg <adrianaisemberg@gmail.com>
2019-10-19 09:37:09 -0700

    contact

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -9,18 +9,25 @@ export class Contact extends React.Component {
     window.scrollTo(0, 0)
     return (
       <div className={classNames("content", "column")}>
-        <Title text="Contact"/>
-        <p className="contact-intro">I’m here to help you gain or strengthen your parenting gut!</p>
+        <Title text="Contact" />
+        <div className="contact-intro">
+          <p >I’m here to help you gain or strengthen your parenting gut!</p>
+          <p >Please send me a message. Let me know how can I help you and your baby.</p>
+        </div>
         <form name='contact' action='https://formspree.io/lilachberko@gmail.com' method='POST'>
           <input type="hidden" name="_subject" value="BabyDevStudio Contact" />
           <input type="hidden" name="_cc" value="adrianaisemberg@gmail.com" />
           <div className="input">
             <label htmlFor='form.name'>Name</label>
-            <div><input type='text' name='Name' id='form.name' required/></div>
+            <div><input type='text' name='Name' id='form.name' required /></div>
+          </div>
+          <div className="input">
+            <label htmlFor='form.babyname'>Your baby's name and age, unless still pregnant</label>
+            <div><input type='text' name='Name' id='form.babyname' /></div>
           </div>
           <div className="input">
             <label htmlFor='form.email'>Email</label>
-            <div><input type='email' name='Email' id='form.email' required/></div>
+            <div><input type='email' name='Email' id='form.email' required /></div>
           </div>
           <div className="input">
             <label htmlFor='form.tel'>Phone</label>
