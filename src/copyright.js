05ccb9dee7c604805c299372f59a1720a381f8d6
Adrian Aisemberg <adrianaisemberg@gmail.com>
2019-10-20 00:33:13 -0700

    mailto

--- src/copyright.js
+++ src/copyright.js
@@ -6,7 +6,11 @@ var classNames = require('classnames');
 export class Copyright extends React.Component {
   render() {
     return (
-      <div className={classNames("copyright")}>&copy; Copyright {(new Date().getFullYear())}, Lilach Aisemberg  |  <Link to='/disclaimer'>Disclaimer</Link>  |  <Link to='/terms-of-use'>Terms</Link></div>
+      <div className={classNames("copyright")}>&copy; Copyright {(new Date().getFullYear())}, Lilach Aisemberg  |  <Link to="" onClick={()=>this.mailto()}>lilach@babydevstudio.com</Link>  |  <Link to='/disclaimer'>Disclaimer</Link>  |  <Link to='/terms-of-use'>Terms</Link></div>
     )
+  }
+
+  mailto(){
+    window.location.href = "mailto:lilach@babydevstudio.com"
   }
 }
