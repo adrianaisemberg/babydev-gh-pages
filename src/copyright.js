096841065153f7ff96d0fe78b321e2866f24dc60
Adrian Aisemberg <adrianaisemberg@gmail.com>
2019-10-21 00:40:42 -0700

    fixes

--- src/copyright.js
+++ src/copyright.js
@@ -6,7 +6,10 @@ var classNames = require('classnames');
 export class Copyright extends React.Component {
   render() {
     return (
-      <div className={classNames("copyright")}>&copy; Copyright {(new Date().getFullYear())}, Lilach Aisemberg  |  <Link to="" onClick={()=>this.mailto()}>lilach@babydevstudio.com</Link>  |  <Link to='/disclaimer'>Disclaimer</Link>  |  <Link to='/terms-of-use'>Terms</Link></div>
+      <>
+        <div className={classNames("copyright")}>&copy; Copyright {(new Date().getFullYear())}, Lilach Aisemberg</div>
+        <div className={classNames("copyright", "copyright-links")}><Link to="" onClick={()=>this.mailto()}>lilach@babydevstudio.com</Link>  |  <Link to='/disclaimer'>Disclaimer</Link>  |  <Link to='/terms-of-use'>Terms</Link></div>
+      </>
     )
   }
 
