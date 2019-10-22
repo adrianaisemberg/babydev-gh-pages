05d154396ac67fbb71e6c76828e6410d708c917b
Adrian Aisemberg <adrianaisemberg@gmail.com>
2019-10-21 22:41:05 -0700

    c

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -18,23 +18,23 @@ export class Contact extends React.Component {
         <form name='contact' action='https://getform.io/f/592d77a3-8405-494b-83be-dbcb8ce2041b' method='POST'>
           <div className="input">
             <label htmlFor='form.name'>Name</label>
-            <div><input type='text' name='Name' id='form.name' required /></div>
+            <input type='text' name='Name' id='form.name' required />
           </div>
           <div className="input">
             <label htmlFor='form.babyname'>Your baby's name and age, unless still pregnant</label>
-            <div><input type='text' name='Baby' id='form.babyname' /></div>
+            <input type='text' name='Baby' id='form.babyname' />
           </div>
           <div className="input">
             <label htmlFor='form.email'>Email</label>
-            <div><input type='email' name='Email' id='form.email' required /></div>
+            <input type='email' name='Email' id='form.email' required />
           </div>
           <div className="input">
             <label htmlFor='form.tel'>Phone</label>
-            <div><input type='tel' name='Phone' id='form.tel' /></div>
+            <input type='tel' name='Phone' id='form.tel' />
           </div>
           <div className="input">
             <label htmlFor='form.text'>Message</label>
-            <div><textarea name='message' id='form.message' rows={10} /></div>
+            <textarea name='message' id='form.message' rows={10} />
           </div>
           <div className="input">
             <input type='submit' value='Send' />
