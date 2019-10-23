2ba3842fde0100a0b0604f87dfa7e0c289ce9676
Adrian Aisemberg <adrianaisemberg@gmail.com>
2019-10-22 23:47:34 -0700

    col

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 import * as React from 'react';
 import { Routes } from './routes';
+var classNames = require('classnames');
 
 export class Main extends React.Component {
   render() {
     window.scrollTo(0, 0)
     return (
-      <main className="content">
+      <main className={classNames("content", "column")}>
         <Routes />
       </main>
     )
